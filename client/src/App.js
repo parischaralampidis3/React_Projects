ca328f6e29fc39a4f16b971913c1f0850cc9834b
Paris Charalampidis <curlycode@outlook.com>
2021-10-26 19:26:12 +0300

    front connected to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from  'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState(" ");
+
+  useEffect(() =>{
+    fetch("http://localhost:9000/testAPI")
+    .then(res => res.text())
+    .then(res => setState({apiResponse: res} ))
+    .catch(error => console.log('error'))
+ 
+  },[])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello client</h1>
+      <p>{state.apiResponse}</p>
     </div>
   );
 }
